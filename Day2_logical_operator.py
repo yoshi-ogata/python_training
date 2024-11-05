0873b77a4719834c849ffb0594074c93bcb50880
yoshi-ogata <yoshihiro_ogata@niandc.co.jp>
2024-11-05 10:14:40 +0530

    Comparison  operators

---
+++ Day2_logical_operator.py
@@ -0,0 +1,15 @@
+# Addition
+print(5 + 3)
+# Subtraction
+print(10 - 2)
+# Multiplication
+print(4 * 2)
+# Division
+print(16 / 2)
+
+#logical operators
+
+#and -> True (Both True)
+#or -> True (Any True)
+#not -> Opposite
+
